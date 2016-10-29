141778b93cb3247b57d7e5fbfc87c32f9c53d767
marto ndegz <martondegz@gmail.com>
2016-10-29 23:03:55 +0300

    updaye

--- greensock-image-slideshow/js/main.js
+++ greensock-image-slideshow/js/main.js
@@ -25,15 +25,28 @@
 
 		// Go to next slide
 	function goToNextSlide(slideOut, slideIn){
+		var tl = new TimelineLite(),
+		 	slideOutH1 = slideOut.find('h1'),
+		 	slideOutP = slideOut.find('p'),
+		 	slideInH1 = slideIn.find('h1'),
+		 	slideInP = slideIn.find('p');
 
-		
+		//go to the next slide timeline
+		tl 
+			.set(slideIn, {y: 100%, autoAlpha: 1, className: '+=active'})
+			.set(slideOut, {className: '-=active'})
+			.to(slideOut, 0.5 {y: '-100%', ease:Power3.easeInOut}, 0)
+			.to(slideIn, 0.5 {y: '-=100%', ease:Power3.easeInOut}, 0);
+
+
 	}
 
 		//Navigation Click - go to the next slide
 	$slideNavNext.click(function(e){
 		e.preventDefault();
 
-
+		var slideOut = $('.homeSlide.active'),
+			slideIn = $('.homeSlide.active').next('.homeSlide');
 
 		goToNextSlide(slideOut, slideIn);
 
